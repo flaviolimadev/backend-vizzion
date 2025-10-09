3d215c5a674534c4de776753edaf8fc35d5f85f8
Flavio Lima <flavio.pessoalbr@gmail.com>
2025-10-09 09:34:43 -0300

    12as

--- src/modules/support/support.module.ts
+++ src/modules/support/support.module.ts
@@ -5,9 +5,14 @@ import { TicketMessage } from './entities/ticket-message.entity';
 import { SupportService } from './support.service';
 import { UserTicketsController } from './user-tickets.controller';
 import { AdminTicketsController } from './admin-tickets.controller';
+import { User } from '../user/entities/user.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Ticket, TicketMessage])],
+  imports: [
+    TypeOrmModule.forFeature([Ticket, TicketMessage, User]),
+    AuthModule,
+  ],
   controllers: [UserTicketsController, AdminTicketsController],
   providers: [SupportService],
 })
